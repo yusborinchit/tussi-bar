fec963c1bd39d92abaf1ec8f6314865b9e76398e
Santiago Garcia <just.b000ringsh1t@gmail.com>
2024-08-14 02:46:48 -0300

    feat: :tada: Basic app features

---
+++ src/app.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import SpotifyIcon from "./components/icons/spotify-icon";
+import { bars } from "./data/bars";
+
+interface Bar {
+  first_bar: string;
+  second_bar: string;
+  song_title: string;
+  url: string;
+}
+
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomBar() {
+  const randomIndex = getRandomNumber(0, bars.length - 1);
+  return bars[randomIndex];
+}
+
+export default function App() {
+  const [bar, setBar] = useState<Bar>(getRandomBar());
+
+  useEffect(() => {
+    setBar(getRandomBar());
+  }, []);
+
+  return (
+    <main className="grid min-h-screen place-items-center bg-zinc-950 px-4 font-new-amsterdam text-zinc-50">
+      <section className="flex items-center">
+        <h1 className="flex flex-col text-7xl leading-[0.75] opacity-10">
+          <span>Tussi</span>
+          <span>Bar</span>
+          <span>of</span>
+          <span>the</span>
+          <span>Day</span>
+        </h1>
+        <div className="-ml-20 flex flex-col items-center text-center font-schoolbell">
+          <p className="relative w-fit max-w-xl text-[38px] leading-[1] tracking-tighter">
+            <span className="text-5xl text-purple-500">"</span>
+            {bar.first_bar}
+          </p>
+          <p className="relative w-fit max-w-xl text-[38px] leading-[1] tracking-tighter">
+            {bar.second_bar}
+            <span className="text-5xl text-purple-500">"</span>
+          </p>
+          <a
+            href={bar.url}
+            target="_blank"
+            className="ml-auto mt-6 flex items-center gap-1 opacity-60"
+          >
+            <SpotifyIcon width={42} height={42} />
+            <span className="font-sans text-xl tracking-tighter opacity-60">
+              Escucha "{bar.song_title}" acá
+            </span>
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+}
